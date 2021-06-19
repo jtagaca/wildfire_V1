f75be3585f8d910773e1d69ec8e978cc9499dbcd
jtagaca <jtagaca@csub.edu>
2021-06-18 20:53:47 -0700

    styling updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-
-
 const Header = () => {
-    return (
-        <header className="header">
-            <h1> Wildfire Tracker (Data provided by NASA)</h1>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1> Wildfire</h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
